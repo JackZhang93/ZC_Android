6903065cdb3e3ffb21aeab20e98425a54f139015
cheng.zhang <zhang93@126.com>
2018-02-05 09:53:26 +0800

    填写银行卡信息

---
+++ app/src/main/java/com/uns/uu/uupaymentsdk/InputBindCardActivity.kt
@@ -0,0 +1,20 @@
+package com.uns.uu.uupaymentsdk
+
+/**
+ * author: 张承
+ * time：2018/2/2
+ * des：输入绑定银行卡信息
+ */
+
+class InputBindCardActivity : BaseActivity() {
+    override fun getLayout(): Int {
+        return R.layout.activity_input_bind_card
+    }
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+    }
+
+}
